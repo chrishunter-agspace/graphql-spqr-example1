1bc9618d5bb6aa084eea1010f2de14f4612c6190
Chris Hunter <chris.hunter@ag-space.com>
2017-07-18 11:24:18 +0100

    Added location to user, which now fails

---
+++ src/main/java/com/agspace/example/configuration/JacksonConfiguration.java
@@ -0,0 +1,19 @@
+package com.agspace.example.configuration;
+
+import com.bedatadriven.jackson.datatype.jts.JtsModule;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class JacksonConfiguration {
+
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper().findAndRegisterModules();
+        mapper.registerModule(new JtsModule());
+        return mapper;
+    }
+}
--- src/main/java/com/agspace/example/entity/User.java
+++ src/main/java/com/agspace/example/entity/User.java
@@ -1,7 +1,11 @@
 package com.agspace.example.entity;
 
+import com.vividsolutions.jts.geom.Geometry;
+
 public class User {
     private String name;
+
+    private Geometry location;
 
     public String getName() {
         return name;
@@ -9,5 +13,13 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Geometry getLocation() {
+        return location;
+    }
+
+    public void setLocation(Geometry location) {
+        this.location = location;
     }
 }
